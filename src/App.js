cb1fda5e6d9de82a758c305fc08a856976447b30
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-14 14:54:07 -0300

    pasting original text if none is provided

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React,{useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 function App() {
   const [text, setText] = useState("");
-  const[correctedText,setCorrectedText] = useState('');
+  const [correctedText, setCorrectedText] = useState("");
+  const [textToRemove, setTextToRemove] = useState("");
 
-  const handleRemotion= (e)=>{
+  const handleRemotion = (e) => {
     e.preventDefault();
-    const firstLine = text.split("\n")[0];
-    console.log(firstLine);
-  }
+    const textArray = text.split("\n");
+    if (textToRemove) {
+      const filteredTextArray = textArray.filter((line) => {
+        return !line.includes(textToRemove);
+      });
+      const correction = filteredTextArray.join("\n");
+      setCorrectedText(correction);
+    } else {
+      setCorrectedText(text);
+    }
+  };
   return (
     <div className="App">
       <h1>Digite o Texto Abaixo</h1>
@@ -19,12 +28,15 @@ function App() {
         }}
       />
       <h1>As linhas que você quer remover contém</h1>
-      <input/>
-      <button onClick={e=>handleRemotion(e)}>Remover</button>
-      <h1>Texto Corrigido</h1>
-      <textarea
-        value={correctedText}
+      <input
+        value={textToRemove}
+        onChange={(e) => {
+          setTextToRemove(e.target.value);
+        }}
       />
+      <button onClick={(e) => handleRemotion(e)}>Remover</button>
+      <h1>Texto Corrigido</h1>
+      <textarea value={correctedText} />
     </div>
   );
 }
